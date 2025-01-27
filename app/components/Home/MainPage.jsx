7df1e08792d564a5058f71dccd430a0271a7f924
Mahfujur-Rahman-0 <mdjeem86@gmail.com>
2025-01-27 13:39:02 +0600

    Mouse updated

--- app/components/Home/MainPage.jsx
+++ app/components/Home/MainPage.jsx
@@ -1,11 +1,11 @@
 "use client";
 import Image from "next/image";
-import { useEffect, useRef } from "react";
-import TorchlightEffect from "../Test";
+import { useEffect, useRef, useState } from "react";
 
 export default function MainPage() {
 	const videoRef = useRef(null);
-	console.log(videoRef);
+	const [Mposition, setMposition] = useState({ x: 0, y: 0 });
+
 	useEffect(() => {
 		const video = videoRef.current;
 
@@ -13,19 +13,11 @@ export default function MainPage() {
 			video.play().catch((err) => console.error("Autoplay error:", err));
 		}
 	}, []);
-	// Function to update CSS variables for mouse position
-	useEffect(() => {
-		const handleMouseMove = (event) => {
-			const root = document.documentElement;
-			root.style.setProperty("--hero-mask-x", `${event.clientX}px`);
-			root.style.setProperty("--hero-mask-y", `${event.clientY}px`);
-		};
 
-		window.addEventListener("mousemove", handleMouseMove);
-		return () => {
-			window.removeEventListener("mousemove", handleMouseMove);
-		};
-	}, []);
+	const handleMouseMove = (event) => {
+		setMposition({ x: event.clientX, y: event.clientY });
+	};
+
 	return (
 		<>
 			<section>
@@ -41,19 +33,31 @@ export default function MainPage() {
 					>
 						<source src="/video/showreAnimation.mp4" type="video/mp4"></source>
 					</video>
+
 					<Image
 						src="/ImageOne.svg"
 						width={1920}
 						height={1080}
 						alt="Torchlight effect"
-						className="w-full h-full absolute top-0 right-[-9%] object-cover"
+						className="w-full h-full transition-all absolute top-0 right-[-9%] object-cover mask-radial-spotlight"
+						style={{
+							"--hero-mask-size": "100px",
+							"--hero-mask-x": Mposition.x - 50 + "px",
+							"--hero-mask-y": Mposition.y - 50 + "px",
+						}}
 					/>
 					<Image
+						onMouseMove={handleMouseMove}
 						src="/ImgTwo.svg"
 						width={1920}
 						height={1080}
 						alt="Torchlight effect"
-						className="w-full h-full absolute top-0 right-[-9%] object-cover"
+						className="w-full h-full transition-all absolute top-0 right-[-9%] object-cover mask-radial-spotlight"
+						style={{
+							"--hero-mask-size": "150px",
+							"--hero-mask-x": `${Mposition.x - 75}px`,
+							"--hero-mask-y": `${Mposition.y - 75}px`,
+						}}
 					/>
 				</div>
 			</section>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				background: "var(--background)",
+				foreground: "var(--foreground)",
+			},
+			maskImage: {
+				"radial-spotlight":
+					"radial-gradient(var(--hero-mask-size) at var(--hero-mask-x) var(--hero-mask-y), black 20%, transparent)",
+			},
+		},
+	},
+	plugins: [
+		function ({ addUtilities }) {
+			addUtilities({
+				".mask-radial-spotlight": {
+					maskImage:
+						"radial-gradient(var(--hero-mask-size) at var(--hero-mask-x) var(--hero-mask-y), black 20%, transparent)",
+					WebkitMaskImage:
+						"radial-gradient(var(--hero-mask-size) at var(--hero-mask-x) var(--hero-mask-y), black 20%, transparent)",
+				},
+			});
+		},
+	],
 };
